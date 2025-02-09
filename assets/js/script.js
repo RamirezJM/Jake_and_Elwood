1410eb885983fcf0d0045a69bcc14999a5777325
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-02-08 23:43:55 -0300

    JS functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,16 @@ const navLinks = document.querySelector('.nav-links')
 
 menuButton.addEventListener('click', () => {
   navLinks.classList.toggle('active')
-  menuButton.setAttribute('aria-expanded', navMenu.classList.contains('active'))
+  menuButton.setAttribute('aria-expanded', navLinks.classList.contains('active'))
 
+})
+
+const currentPage = window.location.pathname
+const menuLinks = document.querySelectorAll('nav ul li a')
+
+menuLinks.forEach(link => {
+  const linkPath = link.getAttribute('href')
+  if(currentPage.includes(linkPath)){
+    link.classList.add('active-link')
+  }
 })
